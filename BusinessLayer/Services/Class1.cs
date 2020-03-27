23795a94b438897c8833fae6e445072c46911702
itdev-lenovo <it1devops@gmail.com>
2020-03-27 17:11:26 +0530

    files added

---
+++ BusinessLayer/Services/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Services
+{
+   public  class Class1
+    {
+        //Create a simple model with a single tableand create database for the ASP .Net application. 
+       // a.Create table model and add it to the DB context.
+     // b.Plugin database creation into start up process.
+        //c.Create and add data to the database.Retrieve the data using query. Run the application.
+        //use interface 
+
+    }
+}
